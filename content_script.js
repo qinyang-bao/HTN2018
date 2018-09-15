b760773da8890fee468f409601e2cf9c6aa48a3a
qinyang <q7bao@edu.uwaterloo.ca>
2018-09-15 12:08:01 -0400

    update

--- content_script.js
+++ content_script.js
@@ -1,18 +1,44 @@
 (function() {
-
+	//include jquery
 	var script = document.createElement('script');
 	script.setAttribute("scr", "jquery-3.3.1.min.js");
 	document.head.appendChild(script);
+
+
+	var comment = "";
+
+	//add listener for keypress
+	$(document).keypress(function(e) {
+	  key = String.fromCharCode(e.which);
+	  comment += key;
+	  console.log(key, comment);
+
+	  if (comment.includes(".")){
+	  	//var selector = ":contains("+ comment +")";
+	  	//activeElement = $(selector);
+	  	//console.log("element: ", activeElement.html());
+	  	var activeElement = $(":focus");
+		console.log(activeElement.html());
+	  }
+	  
+
+	});
 	
 
-	//p = $("title").html();
-	//console.log(p);
+
+	/*
 	setInterval(function() {
-  	var activeElement = document.activeElement;
-  	console.log(activeElement.tagName);
-	}, 5000);
+	var activeElement = $(":focus");
+	console.log(activeElement.html());
 	
+	var inputElement = "";
+	activeElement.find($("span").each(function(index ){
+                   console.log( index + ": " + $( this ).text() );
+                }));
 
-
+  	//console.log(inputElement.innerHTML);
+	}, 5000);
+	*/
+	
 
 })();
