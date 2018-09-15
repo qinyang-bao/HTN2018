dacb81667c948f4c0e613966f84ee744c3d3bcb6
Shawn Sui <shawnsui@v1020-wn-148-5.campus-dynamic.uwaterloo.ca>
2018-09-15 14:51:58 -0400

    added document tone

--- content_script.js
+++ content_script.js
@@ -4,8 +4,9 @@
 	script.setAttribute("scr", "jquery-3.3.1.min.js");
 	document.head.appendChild(script);
 
-
+	// anger level: (0) <0.5 (1) 0.5 - 0.7 (2) 0.7 - 0.9 (3) >0.9
 	let comment = "";
+	let sentenceTone = [];
 
 	//add listener for keypress
 	$(document).keypress(function(e) {
@@ -26,10 +27,25 @@
 				"Authorization": "Basic " + btoa("3c86c40d-60e5-43d9-9215-2ceea54b6fad" + ":" + "tXbZalBwWS8e")
 			},
 			success: function(data) {
-			  //called when successful
-	
-			  console.log(data);
-	
+			let documentToneAnger = "";
+
+			//return array if has anger
+			let docToneData = data.document_tone.tones.filter(tone => tone.tone_id === "anger");
+			console.log(docToneData);
+			if (docToneData[0].score >= 0.5 && docToneData[0].score < 0.7) {
+				documentToneAnger = 1;
+			} else if (docToneData[0].score >= 0.7 && docToneData[0].score < 0.9) {
+				documentToneAnger = 2;
+			} else if (docToneData[0].score >= 0.9) {
+				documentToneAnger = 3;
+			} else {
+				documentToneAnger = 0;
+			}
+			
+			//let sentenceToneData = data.document_tone.tones.filter(tone => tone.tone_id === "anger");
+
+			console.log("data: ", data);
+			console.log("docAnger: ", documentToneAnger);
 			},
 			error: function(e) {
 			  //called when there is an error
