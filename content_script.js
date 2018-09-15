edf146aec5f5ef75aca0d16defe68e5afabd5be7
Shawn Sui <shawnsui@v1020-wn-148-5.campus-dynamic.uwaterloo.ca>
2018-09-15 14:17:55 -0400

    get text input to request api

--- content_script.js
+++ content_script.js
@@ -5,7 +5,7 @@
 	document.head.appendChild(script);
 
 
-	var comment = "";
+	let comment = "";
 
 	//add listener for keypress
 	$(document).keypress(function(e) {
@@ -13,12 +13,29 @@
 	  comment += key;
 	  console.log(key, comment);
 
-	  if (comment.includes(".")){
+	  if (key === "."){
 	  	//var selector = ":contains("+ comment +")";
 	  	//activeElement = $(selector);
-	  	//console.log("element: ", activeElement.html());
-	  	var activeElement = $(":focus");
-		console.log(activeElement.html());
+		  //console.log("element: ", activeElement.html());
+		let apitext = comment.split(" ").join("%20")
+	  	$.ajax({
+			type: "GET",
+			url: "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21&text=" + apitext,
+			dataType : 'json',
+			headers: {
+				"Authorization": "Basic " + btoa("3c86c40d-60e5-43d9-9215-2ceea54b6fad" + ":" + "tXbZalBwWS8e")
+			},
+			success: function(data) {
+			  //called when successful
+	
+			  console.log(data);
+	
+			},
+			error: function(e) {
+			  //called when there is an error
+			  console.log(e);
+			}
+		});
 	  }
 	  
 
@@ -41,28 +58,6 @@
 	*/
 	
 
-<<<<<<< HEAD
 	p = $("title").html();
 	console.log(p);
-
-	$.ajax({
-		type: "GET",
-		url: "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2017-09-21&text=Team,%20I%20know%20that%20times%20are%20tough!%20Product%20sales%20have%20been%20disappointing%20for%20the%20past%20three%20quarters.%20We%20have%20a%20competitive%20product,%20but%20we%20need%20to%20do%20a%20better%20job%20of%20selling%20it!",
-		dataType : 'json',
-		headers: {
-			"Authorization": "Basic " + btoa("3c86c40d-60e5-43d9-9215-2ceea54b6fad." + ":" + "tXbZalBwWS8e")
-		},
-		success: function(data) {
-		  //called when successful
-
-		  console.log(data);
-
-		},
-		error: function(e) {
-		  //called when there is an error
-		  console.log(e);
-		}
-	});
-=======
->>>>>>> b760773da8890fee468f409601e2cf9c6aa48a3a
 })();
