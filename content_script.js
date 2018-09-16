0bf8d2607673e97dc8e97f1e40c643f0acdd7777
Shawn Sui <shawnsui@v1020-wn-148-5.campus-dynamic.uwaterloo.ca>
2018-09-15 23:12:32 -0400

    added warning label

--- content_script.js
+++ content_script.js
@@ -35,7 +35,6 @@
 							let angerlevel = evaluate_data(data);
 							console.log("angerlevel", angerlevel);
 							update_ele(fb_get_active_ele, angerlevel.document);
-					
 						},
 						error: function(e) {
 						  //called when there is an error
@@ -43,7 +42,6 @@
 						}
 					});
 		}
-
 		else{
 			update_ele(fb_get_active_ele, 3);
 		}
@@ -52,49 +50,56 @@
 
     function evaluate_data(data){
 		// anger level: (0) <0.5 (1) 0.5 - 0.7 (2) 0.7 - 0.9 (3) >0.9
-		let documentToneAnger = "";
-		
-		
-		let docToneData = data.document_tone.tones.filter(tone => tone.tone_id === "anger");
-		if (docToneData.length !=0 ){
-			if (docToneData[0].score >= 0.5 && docToneData[0].score < 0.7) {
-				documentToneAnger = 1;
-			} else if (docToneData[0].score >= 0.7 && docToneData[0].score < 0.9) {
-				documentToneAnger = 2;
-			} else if (docToneData[0].score >= 0.9) {
-				documentToneAnger = 3;
-			} else {
-				documentToneAnger = 0;
-			}
-			console.log("docAnger:", documentToneAnger);
+		let documentToneAnger = 0;
 
-			if (typeof data.sentences_tone != "undefined") {
-				let sentenceToneData = data.sentences_tone
-				.map(sentence => ({text: sentence.text, tone: sentence.tones.filter(tone => tone.tone_id === "anger")[0]}))
-				.map(sentence => {
-					if (sentence.tone == undefined) {
-						return ({text: sentence.text, tone: 0})
-					} else if (sentence.tone.score >= 0.5 && sentence.tone.score < 0.7) {
-						return ({text: sentence.text, tone: 1});
-					} else if (sentence.tone.score >= 0.7 && sentence.tone.score < 0.9) {
-						return ({text: sentence.text, tone: 2});
-					} else if (sentence.tone.score >= 0.9) {
-						return ({text: sentence.text, tone: 3});
-					} else {
-						return ({text: sentence.text, tone: 0});
-					}
-				})
-				return {document: documentToneAnger, sentences: sentenceToneData}
-			}
-			return {document: documentToneAnger, sentences: undefined}
+		// document anger
+		let docToneData = data.document_tone.tones.filter(tone => tone.tone_id === "anger");
+		if (docToneData.length === 0) {
+			documentToneAnger = 0;
+		} else if (docToneData[0].score >= 0.5 && docToneData[0].score < 0.7) {
+			documentToneAnger = 1;
+		} else if (docToneData[0].score >= 0.7 && docToneData[0].score < 0.9) {
+			documentToneAnger = 2;
+		} else if (docToneData[0].score >= 0.9) {
+			documentToneAnger = 3;
+		} else {
+			documentToneAnger = 0;
 		}
+
+		// sentences anger
+		if (typeof data.sentences_tone != "undefined") {
+			let sentenceToneData = data.sentences_tone
+			.map(sentence => ({text: sentence.text, tone: sentence.tones.filter(tone => tone.tone_id === "anger")[0]}))
+			.map(sentence => {
+				if (sentence.tone == undefined) {
+					return ({text: sentence.text, tone: 0})
+				} else if (sentence.tone.score >= 0.5 && sentence.tone.score < 0.7) {
+					return ({text: sentence.text, tone: 1});
+				} else if (sentence.tone.score >= 0.7 && sentence.tone.score < 0.9) {
+					return ({text: sentence.text, tone: 2});
+				} else if (sentence.tone.score >= 0.9) {
+					return ({text: sentence.text, tone: 3});
+				} else {
+					return ({text: sentence.text, tone: 0});
+				}
+			})
+			return {document: documentToneAnger, sentences: sentenceToneData}
+		}
+		return {document: documentToneAnger, sentences: undefined}
 	}
 
     function update_ele(get_active_ele, data){
-    	ele = get_active_ele();
-    	if (typeof(ele) != "undefined"){
-    		ele.style.color = "red";
-    		ele.setAttribute("id", "_the_one_and_the_only_one_selected");
+		ele = get_active_ele();
+		console.log("ele", ele);
+    	if (typeof(ele) != "undefined" && data > 0){
+    		//ele.style.color = "red";
+			//ele.setAttribute("id", "_the_one_and_the_only_one_selected");
+			ele.setAttribute("id", "warning");
+			let div = document.createElement("div");
+			document.getElementById("warning").prepend(div);
+			div.setAttribute("style", "width: 100px; height: 30px; background-color: red; color: white; font-size: 12px");
+			div.innerText = "!Warning";
+			console.log(ele);
     	}
     }
 
@@ -119,7 +124,6 @@
 
 	//add listener for keypress
 	$(document).keydown(function(e) {
-	  console.log("raw", e.keyCode, e.key);
 	  if (valid_key(e.keyCode)){
 	  	var key = e.key;
 	  	if (key != "Backspace"){
@@ -130,8 +134,6 @@
 	  			comment = comment.substring(0, comment.length - 1);
 	  		}
 	  	}
-	  	
-	  	console.log(key, comment);
 
 	  	if (key == "."){
 	  		call_api(comment);
@@ -140,19 +142,4 @@
 	  }
 
 	});
-	
- 
-	/*
-	setInterval(function() {
-	var activeElement = $(":focus");
-	console.log(activeElement.html());
-	
-	var inputElement = "";
-	activeElement.find($("span").each(function(index ){
-                   console.log( index + ": " + $( this ).text() );
-                }));
-  	//console.log(inputElement.innerHTML);
-	}, 5000);
-	*/
-	
 })();
